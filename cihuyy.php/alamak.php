4bd2bf6a2d247e4336065bbab95a58af63ae69ac
ardantresna <jelaszrokece@gmail.com>
2024-11-04 15:01:43 +0700

    instal commit

---
+++ cihuyy.php/alamak.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document Layout</title>
+</head>
+<body>
+    <table border="1" width="100%" height="1000px">
+        <tr>
+            <td colspan="2" align="center">Header</td>
+        </tr>
+        <tr>
+            <td width="10%" align="center">Sidebar</td>
+            <td align="center">Main Content</td>
+        </tr>
+        <tr>
+            <td colspan="2" align="center"height="200%">Footer</td>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ ular python/main.py
@@ -0,0 +1,1 @@
+print("Masukkan ular python");
